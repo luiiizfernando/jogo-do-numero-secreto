a8801b0797492510dbf0469fc7b8afa7e859a6c7
Luiz Fernando <luizfs138@gmail.com>
2024-01-25 13:17:03 -0300

    Atualização do jogo

--- app.js
+++ app.js
@@ -25,34 +25,44 @@ function exibirTextoNaTela(tag, texto)
 function exibirMensagemInicial() 
 {
     exibirTextoNaTela("h1", "Jogo do número secreto");
-    exibirTextoNaTela("p", "Escolha um número entre 1 e 10:");    
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 10:");
+
 }
 
 
 function verificarChute()
 {
     let chute = document.querySelector("input").value;
-    
-    if (chute == numeroSecreto)
+
+    if(chute < 1 || chute > 10)
     {
-        exibirTextoNaTela("h1", "Acertou!");
-        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela("p", mensagemTentativas);
-        document.getElementById("reiniciar").removeAttribute("disabled");
+        exibirTextoNaTela("p", "Número inválido. Digite um novo número:");
+        limparCampo();
     }
     else
     {
-        if (chute > numeroSecreto)
-        {
-            exibirTextoNaTela("p", "O número secreto é menor.");
+    if (chute == numeroSecreto)
+    {
+            exibirTextoNaTela("h1", "Acertou!");
+            let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+            let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+            exibirTextoNaTela("p", mensagemTentativas);
+            document.getElementById("tentar").disabled = true;
+            document.getElementById("reiniciar").removeAttribute("disabled");
         }
         else
         {
-            exibirTextoNaTela("p", "O número secreto é maior.")
+            if (chute > numeroSecreto)
+            {
+                exibirTextoNaTela("p", "O número secreto é menor.");
+            }
+            else
+            {
+                exibirTextoNaTela("p", "O número secreto é maior.")
+            }
+            tentativas++;
+            limparCampo();
         }
-        tentativas++;
-        limparCampo();
     }
 }
 
@@ -90,4 +100,5 @@ function reiniciarJogo()
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById("reiniciar").setAttribute("disabled", true);
+    document.getElementById("tentar").disabled = false;
 }
